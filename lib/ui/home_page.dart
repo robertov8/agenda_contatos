1a4988150b67a20af72aba56f0c53eef35f3490a
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-23 23:13:19 -0300

    83 - Criando um Botão Flutuante

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,28 +8,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
 
   @override
   void initState() {
     super.initState();
 
-    /*Contact c = Contact();
-    c.name = 'RobertoRibeiro';
-    c.email = 'robertov82008@gmail.com';
-    c.phone = '1234567890';
-    c.img = 'imgtest';
-
-    helper.saveContact(c);*/
-
     helper.getAllContacts().then((list) {
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contatos'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {},
+      ),
+    );
   }
 }
