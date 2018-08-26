28e411f55863b6bd60b676196dbc0b51cc2adaee
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-26 19:18:10 -0300

    88 - Retornando o Contato Modificado

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -19,6 +19,8 @@ class _ContactPageState extends State<ContactPage> {
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
 
+  final _nameFocus = FocusNode();
+
   @override
   void initState() {
     super.initState();
@@ -43,7 +45,13 @@ class _ContactPageState extends State<ContactPage> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+            Navigator.pop(context, _editedContact);
+          } else {
+            FocusScope.of(context).requestFocus(_nameFocus);
+          }
+        },
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
       ),
@@ -66,6 +74,7 @@ class _ContactPageState extends State<ContactPage> {
             TextField(
               decoration: InputDecoration(labelText: 'Nome'),
               controller: _nameController,
+              focusNode: _nameFocus,
               onChanged: (text) {
                 _userEdited = true;
 
